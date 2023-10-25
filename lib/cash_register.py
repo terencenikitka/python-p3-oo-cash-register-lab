64ca2fbe42a3ecce4d25fbd1b2ef8ba19ab5ccc3
Kevin <staffordkevinjames@gmail.com>
2023-10-25 15:28:53 -0400

    ADDS: Methods to complete lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+
+  def add_item(self, title, price, quantity=1):
+    self.title = title
+    self.price = price
+    self.quantity = quantity
+    self.total += price * quantity
+    for x in range(quantity):
+      self.items.append(title)
+
+  def apply_discount(self):
+    if self.discount:
+      self.total = self.total * (1 - (self.discount / 100))
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    self.total -= self.price * self.quantity
+
+
   pass
+
+cash_register = CashRegister()
+cash_register_with_discount = CashRegister(20)
